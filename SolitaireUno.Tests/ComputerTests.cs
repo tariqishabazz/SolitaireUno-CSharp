b7a4d53a2dc57d20130fb870d5ae80eaccb288df
Tariq <htdst@LIVING-ROOM-PC>
2026-05-28 15:44:25 -0400

    minor changes

--- SolitaireUno.Tests/ComputerTests.cs
+++ SolitaireUno.Tests/ComputerTests.cs
@@ -1,16 +1,21 @@
 ﻿namespace SolitaireUno.Tests
 {
+    /// <summary>
+    /// Contains unit tests for verifying the behavior of the Computer class.
+    /// </summary>
     public class ComputerTests
     {
         [Fact]
         public void Computer_Correctly_InitializesWith_10_Cards()
         {
+            // ARRANGE
             Deck testDeck = new Deck();
-
             Computer computer = new Computer(testDeck);
-
+            
+            // ACT 
             int computerHandCount = computer.Hand.Count;
-
+            
+            // ASSERT
             Assert.Equal(10, computerHandCount);
         }
     }
--- SolitaireUno.Tests/PlayerTests.cs
+++ SolitaireUno.Tests/PlayerTests.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace SolitaireUno.Tests
 {
+    /// <summary>
+    /// Contains unit tests for verifying the behavior of the Player class.
+    /// </summary>
+    /// <remarks>This test class uses the xUnit testing framework to ensure that the Player class initializes
+    /// and functions as expected.</remarks>
     public class PlayerTests
     {
         [Fact]
